48c77925ead80dadc9192556fb3fa63f0ae86cc4
Sid <saytleader@outlook.com>
2023-02-02 14:59:56 -0500

    more debugging

--- viewer.js
+++ viewer.js
@@ -1,12 +1,12 @@
 function getSiteLabel() {
-    console.log("");
+    console.log("works");
     var label = document.getElementById("sitelabel");
     const queryString = window.location.search;
     console.log(queryString);
     //const url = queryString.slice()
 }
 function loadSite() {
-    console.log("");
+    console.log("works");
     var box = document.getElementById("pagebox");
     const queryString = window.location.search;
 }
