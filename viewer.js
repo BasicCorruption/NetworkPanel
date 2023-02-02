02ed47c5876e4a01394bedc7b25d8774cb308df1
Sid <saytleader@outlook.com>
2023-02-02 14:45:45 -0500

    add some code

--- viewer.js
+++ viewer.js
@@ -1,4 +1,10 @@
 function getSiteLabel() {
     var label = document.getElementById("sitelabel");
     const queryString = window.location.search;
+    console.log(queryString);
+    //const url = queryString.slice()
+}
+function loadSite() {
+    var box = document.getElementById("pagebox");
+    const queryString = window.location.search;
 }
