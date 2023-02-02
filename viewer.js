e438eed144474bd175106a90087085f45f8d3815
Sid <saytleader@outlook.com>
2023-02-02 14:56:28 -0500

    add debugging code

--- viewer.js
+++ viewer.js
@@ -1,10 +1,12 @@
 function getSiteLabel() {
+    console.log("");
     var label = document.getElementById("sitelabel");
     const queryString = window.location.search;
     console.log(queryString);
     //const url = queryString.slice()
 }
 function loadSite() {
+    console.log("");
     var box = document.getElementById("pagebox");
     const queryString = window.location.search;
 }
