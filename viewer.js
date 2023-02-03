6f12a4c58566ac2c8f18787056a9dd876e920eb8
Sid <saytleader@outlook.com>
2023-02-03 12:47:06 -0500

    add iframe box

--- viewer.js
+++ viewer.js
@@ -21,4 +21,17 @@ function getSiteLabel() {
 function loadSite() {
     var box = document.getElementById("pagebox");
     const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    const url = params.get("url");
+
+    var regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+    if (url.match(regex)) {
+        console.log("Parameter is URL");
+    } else {
+        console.log("Parameter is not a URL, returning to home page");
+        window.location.href = "index.html";
+        return;
+    }
+
+    box.innerHTML = `<iframe src="${url}"></iframe>`;
 }
