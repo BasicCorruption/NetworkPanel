69a00660778bd5c7f32bc5bf60ddd1276c441df9
Sid <saytleader@outlook.com>
2023-02-03 12:27:55 -0500

    add the site label

--- viewer.js
+++ viewer.js
@@ -6,6 +6,17 @@ function getSiteLabel() {
     console.log(params);
     const url = params.get("url");
     console.log(url);
+
+    var regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+    if (url.match(regex)) {
+        console.log("Parameter is URL");
+    } else {
+        console.log("Parameter is not a URL, returning to home page");
+        window.location.href = "index.html";
+        return;
+    }
+
+    label.innerHTML = `<p>${url}</p>`;
 }
 function loadSite() {
     var box = document.getElementById("pagebox");
